12f5a822bdb741b38ec3220789d9138670474d23
burankar <burankar@gmail.com>
2014-11-14 15:08:59 -0700

    comment outline of the upcoming SC script

---
+++ SCScript.py
@@ -0,0 +1,13 @@
+'''This is a basic script that will output the U.S. Supreme Court makeup based on user input of a year. I'm making it to practice python objects and syntax. it will be updated with more functionality as I get more comfortable with Python.
+'''
+
+#SC object (inputs: self, chief justice, 8 other justices)
+
+#specific instances of SCs working backwards
+
+#elif statement to verify integer input
+
+#elif statements to break up years of courts
+
+
+
