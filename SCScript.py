f16a859060ea3e740c0a46346e24f32f8e3d72c5
burankar <burankar@gmail.com>
2014-11-15 16:37:04 -0700

    this is far cleaner code and should work

--- SCScript.py
+++ SCScript.py
@@ -1,25 +1,6 @@
 '''This is a basic script that will output the U.S. Supreme Court makeup based on user input of a year. I'm making it to practice python objects and syntax. it will be updated with more functionality as I get more comfortable with Python.
 '''
 	
-#specific courts set as empty lists working reverse chronologically
-roberts_court = []
-renquist_court = []
-burger_court = []
-warren_court = []
-vinson_court = []
-stone_court = []
-hughes_court = []
-taft_court = []
-white_court = []
-fuller_court = []
-waite_court = [] 
-chase_court = []
-taney_court = []
-marshall_court = []
-ellsworth_court = []
-rutledge_court = []
-jay_court = []
-
 #elif statement to verify integer input
 
 #Justice class. Each justice is an instance of this
@@ -45,51 +26,17 @@ list_of_justices = [roberts, kagan, sotomayor, alito, breyer, ginsburg, thomas]
 sorts through list of justices. It's close to being complete but still not perfect. If someone raw inputs '2014' it will probably draw the Roberts court with additional justices such as David Souter (served in Roberts court but retired in 2009). I think I need to add a boolean based around raw_input to fix it.
 '''
 
-def justice_sorting(list):
-	for x in list:
-		if x.end_date >= 2005:
-			roberts_court.append(x.name)
-		elif x.end_date <= 2005 && x.end_date >= 1986:
-			rehnquist_court.append(x.name)
-		elif x.end_date <= 1986 && x.end_date >= 1969:
-			burger_court.append(x.name)
-		elif x.end_date <= 1969 && x.end_date >= 1954:
-			warren_court.append(x.name)
-		elif x.end_date <= 1954 && x.end_date >= 1946:
-			vinson_court.append(x.name)
-		elif x.end_date <= 1946 && x.end_date >= 1941:
-			stone_court.append(x.name)
-		elif x.end_date <= 1941 && x.end_date >= 1930:
-			hughes_court.append(x.name)
-		elif x.end_date <= 1930 && x.end_date >= 1921:
-			taft_court.append(x.name)
-		elif x.end_date <= 1921 && x.end_date >= 1910:
-			white_court.append(x.name)
-		elif x.end_date <= 1910 && x.end_date >= 1888:
-			fuller_court.append(x.name)
-		elif x.end_date <= 1888 && x.end_date >= 1874:
-			waite_court.append(x.name)
-		elif x.end_date <= 1874 && x.end_date >= 1864:
-			chase_court.append(x.name)
-		elif x.end_date <= 1864 && x.end_date >= 1835:
-			taney_court.append(x.name)
-		elif x.end_date <= 1835 && x.end_date >= 1801:
-			marshall_court.append(x.name)
-		elif x.end_date <= 1800 && x.end_date >= 1796:
-			ellsworth_court.append(x.name)
-		elif x.end_date <= 1796 && x.end_date >= 1795:
-			rutledge_court.append(x.name)
-		elif x.end_date <= 1795 && x.end_date >= 1789:
-			jay_court.append(x.name)	
-			
-justice_sorting(list_of_justices)	
-print roberts_court
-
-	
-'''
-#elif statements to break up years of courts
 YEAR_INPUT = int(raw_input("Please enter year:"))
+RESULTS = []
 
-if YEAR_INPUT >= 2005:
-'''	
+def calculate_court(list):
+	for x in list:
+		if YEAR_INPUT >= x.begin_date && YEAR_INPUT <= x.end_date:
+			RESULTS.append(x.name)
+
+print YEAR_INPUT			
+calculate_court(list_of_justices)
+
+			
+
 
