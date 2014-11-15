cb52db3eae7d1c102fa59e52873bdd78331984f4
burankar <burankar@gmail.com>
2014-11-15 08:01:08 -0700

    added Supreme Court object

--- SCScript.py
+++ SCScript.py
@@ -1,9 +1,20 @@
 '''This is a basic script that will output the U.S. Supreme Court makeup based on user input of a year. I'm making it to practice python objects and syntax. it will be updated with more functionality as I get more comfortable with Python.
 '''
 
-#SC object (inputs: self, chief justice, 8 other justices)
+#SC object (inputs: self, chief justice, 8 other justices, court conservative rating?)
 
-#specific instances of SCs working backwards
+class = SupremeCourt(self, chief_justice, assoc_justice1, assoc_justice2, assoc_justice3, assoc_justice4, assoc_justice5, assoc_justice6, assoc_justice7, assoc_justice8):
+	self.chief_justice = chief_justice
+    self.assoc_justice1 = assoc_justice1
+	self.assoc_justice2 = assoc_justice2
+	self.assoc_justice3 = assoc_justice3
+	self.assoc_justice4 = assoc_justice4
+	self.assoc_justice5 = assoc_justice5
+	self.assoc_justice6 = assoc_justice6
+	self.assoc_justice7 = assoc_justice7
+	self.assoc_justice8 = assoc_justice8
+
+#specific instances of SCs working reverse chronologically
 
 #elif statement to verify integer input
 
