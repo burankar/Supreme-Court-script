9a1a2fa29dd76c8df9ba08b470b2883b86484c73
burankar <burankar@gmail.com>
2014-11-15 13:42:36 -0700

    got the list sorting logic to work finally

--- SCScript.py
+++ SCScript.py
@@ -1,24 +1,11 @@
 '''This is a basic script that will output the U.S. Supreme Court makeup based on user input of a year. I'm making it to practice python objects and syntax. it will be updated with more functionality as I get more comfortable with Python.
 '''
-
-
-#This thing is already getting unwieldy. I need to make a SC justice class.
-class Justice(object):
 	
-	def __init__(self, name, begin_date, end_date):
-		self.name = name
-		self.begin_date = begin_date
-		self.end_date = end_date
-
-'''
-I need to define a function that will allow for quick substitution of Justices. Justices may serve/leave courts in between CJ terms and creating SC instances for each year would be a PITA.
-'''
-
-
 '''specific courts set as empty lists working reverse chronologically'''
 
 #Roberts Court: 2005 - present
 roberts_court = []
+
 #Renquist Court: 1986 - 2005
 renquist_court = []
 
@@ -69,7 +56,39 @@ jay_court = []
 
 #elif statement to verify integer input
 
-#elif statements to break up years of courts
-#How to handle transitional years?
+#Justice class. Each justice is an instance of this
+class Justice(object):
+	
+	def __init__(self, name, begin_date, end_date):
+		self.name = name
+		self.begin_date = begin_date
+		self.end_date = end_date
 
+#current justices
+roberts = Justice('John Roberts', 2005, 2014)
+kagan = Justice('Elena Kagan', 2010, 2014)
+sotomayor = Justice('Sonia Sotomayor', 2009, 2014)
+alito = Justice('Samuel Alito', 2006, 2014)
+breyer = Justice('Stephen Breyer', 1994, 2014)
+ginsburg = Justice('Ruth Bader Ginsburg', 1994, 2014)
+thomas = Justice('Clarence Thomas', 1991, 2014)
+
+list_of_justices = [roberts, kagan, sotomayor, alito, breyer, ginsburg, thomas]
+
+def justice_sorting(list):
+	for x in list:
+		if x.end_date >= 2005:
+			roberts_court.append(x.name)
+	return roberts_court
+
+justice_sorting(list_of_justices)	
+print roberts_court
+
+	
+'''
+#elif statements to break up years of courts
+YEAR_INPUT = int(raw_input("Please enter year:"))
+
+if YEAR_INPUT >= 2005:
+'''	
 
