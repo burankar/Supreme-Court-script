8daf135ce1b62fa999c4058600a1454654f78dd8
burankar <burankar@gmail.com>
2014-11-15 12:26:47 -0700

    changed program to class justices and append to lists

--- SCScript.py
+++ SCScript.py
@@ -1,59 +1,71 @@
 '''This is a basic script that will output the U.S. Supreme Court makeup based on user input of a year. I'm making it to practice python objects and syntax. it will be updated with more functionality as I get more comfortable with Python.
 '''
 
-#SC object (inputs: self, chief justice, 8 other justices, court conservative rating?)
 
-class SupremeCourt(self, begin_date, end date, chief_justice, assoc_justice1, assoc_justice2, assoc_justice3, assoc_justice4, assoc_justice5, assoc_justice6, assoc_justice7, assoc_justice8):
-	self.begin_date = begin_date #should figure out a way to make begin/end a range
-	self.end_date = end_date
-	self.chief_justice = chief_justice
-    self.assoc_justice1 = assoc_justice1
-	self.assoc_justice2 = assoc_justice2
-	self.assoc_justice3 = assoc_justice3
-	self.assoc_justice4 = assoc_justice4
-	self.assoc_justice5 = assoc_justice5
-	self.assoc_justice6 = assoc_justice6
-	self.assoc_justice7 = assoc_justice7
-	self.assoc_justice8 = assoc_justice8
+#This thing is already getting unwieldy. I need to make a SC justice class.
+class Justice(object):
+	
+	def __init__(self, name, begin_date, end_date):
+		self.name = name
+		self.begin_date = begin_date
+		self.end_date = end_date
+
+'''
+I need to define a function that will allow for quick substitution of Justices. Justices may serve/leave courts in between CJ terms and creating SC instances for each year would be a PITA.
+'''
 
 
-'''specific instances of SCs working reverse chronologically'''
+'''specific courts set as empty lists working reverse chronologically'''
 
 #Roberts Court: 2005 - present
-Roberts_court = SupremeCourt(2005, 2014, 'John Roberts', 'Antonin Scalia', 'Anthony Kennedy', 'Clarence Thomas', 'Ruth Bader Ginsburg', 'Stephen Breyer', 'Samuel Alito', 'Sonia Sotomayor', 'Elena Kagan')
-
+roberts_court = []
 #Renquist Court: 1986 - 2005
-Renquist_court = SupremeCourt(1986, 2005, 'William Rehnquist', 
+renquist_court = []
 
 #Burger Court: 1969 - 1986
+burger_court = []
 
 #Warren Court: 1954 -1969
+warren_court = []
 
 #Vinson Court: 1946 - 1953
+vinson_court = []
 
 #Stone Court: 1941 - 1946
+stone_court = []
 
 #Hughes Court: 1930 - 1041
+hughes_court = []
 
 #Taft Court: 1921 - 1930
+taft_court = []
 
 #White Court: 1910 - 1921
+white_court = []
 
 #Fuller Court: 1888 - 1910
+fuller_court = []
 
 #Waite Court: 1874 - 1888
+waite_court = []
 
 #Chase Court: 1864 - 1873 
+chase_court = []
 
 #Taney Court: 1835 - 1864
+taney_court = []
 
 #Marshall Court: 1801 - 1835
+marshall_court = []
 
 #Ellsworth Court: 1796 - 1800
+ellsworth_court = []
 
 #Rutledge Court: 1795 - 1795
+rutledge_court = []
 
 #Jay Court: 1789 - 1795
+jay_court = []
 
 #elif statement to verify integer input
 
