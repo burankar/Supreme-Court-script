e0a2fa0a7ab01747b13340ffe06adaf47f38cb03
burankar <burankar@gmail.com>
2014-11-15 08:49:36 -0700

    realized I need to figure out how to sub justices in and out of a court

--- SCScript.py
+++ SCScript.py
@@ -3,7 +3,9 @@
 
 #SC object (inputs: self, chief justice, 8 other justices, court conservative rating?)
 
-class SupremeCourt(self, chief_justice, assoc_justice1, assoc_justice2, assoc_justice3, assoc_justice4, assoc_justice5, assoc_justice6, assoc_justice7, assoc_justice8):
+class SupremeCourt(self, begin_date, end date, chief_justice, assoc_justice1, assoc_justice2, assoc_justice3, assoc_justice4, assoc_justice5, assoc_justice6, assoc_justice7, assoc_justice8):
+	self.begin_date = begin_date #should figure out a way to make begin/end a range
+	self.end_date = end_date
 	self.chief_justice = chief_justice
     self.assoc_justice1 = assoc_justice1
 	self.assoc_justice2 = assoc_justice2
@@ -14,12 +16,14 @@ class SupremeCourt(self, chief_justice, assoc_justice1, assoc_justice2, assoc_ju
 	self.assoc_justice7 = assoc_justice7
 	self.assoc_justice8 = assoc_justice8
 
+
 '''specific instances of SCs working reverse chronologically'''
 
 #Roberts Court: 2005 - present
-Roberts_court = SupremeCourt('John Roberts', 'Antonin Scalia', 'Anthony Kennedy', 'Clarence Thomas', 'Ruth Bader Ginsburg', 'Stephen Breyer', 'Samuel Alito', 'Sonia Sotomayor', 'Elena Kagan')
+Roberts_court = SupremeCourt(2005, 2014, 'John Roberts', 'Antonin Scalia', 'Anthony Kennedy', 'Clarence Thomas', 'Ruth Bader Ginsburg', 'Stephen Breyer', 'Samuel Alito', 'Sonia Sotomayor', 'Elena Kagan')
 
 #Renquist Court: 1986 - 2005
+Renquist_court = SupremeCourt(1986, 2005, 'William Rehnquist', 
 
 #Burger Court: 1969 - 1986
 
