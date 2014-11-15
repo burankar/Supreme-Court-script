c75cd178afe1402042282696229c8cb80f7c3f1e
burankar <burankar@gmail.com>
2014-11-15 08:19:44 -0700

    added comment lines to outline each court; added Roberts court

--- SCScript.py
+++ SCScript.py
@@ -3,7 +3,7 @@
 
 #SC object (inputs: self, chief justice, 8 other justices, court conservative rating?)
 
-class = SupremeCourt(self, chief_justice, assoc_justice1, assoc_justice2, assoc_justice3, assoc_justice4, assoc_justice5, assoc_justice6, assoc_justice7, assoc_justice8):
+class SupremeCourt(self, chief_justice, assoc_justice1, assoc_justice2, assoc_justice3, assoc_justice4, assoc_justice5, assoc_justice6, assoc_justice7, assoc_justice8):
 	self.chief_justice = chief_justice
     self.assoc_justice1 = assoc_justice1
 	self.assoc_justice2 = assoc_justice2
@@ -14,11 +14,46 @@ class = SupremeCourt(self, chief_justice, assoc_justice1, assoc_justice2, assoc_
 	self.assoc_justice7 = assoc_justice7
 	self.assoc_justice8 = assoc_justice8
 
-#specific instances of SCs working reverse chronologically
+'''specific instances of SCs working reverse chronologically'''
+
+#Roberts Court: 2005 - present
+Roberts_court = SupremeCourt('John Roberts', 'Antonin Scalia', 'Anthony Kennedy', 'Clarence Thomas', 'Ruth Bader Ginsburg', 'Stephen Breyer', 'Samuel Alito', 'Sonia Sotomayor', 'Elena Kagan')
+
+#Renquist Court: 1986 - 2005
+
+#Burger Court: 1969 - 1986
+
+#Warren Court: 1954 -1969
+
+#Vinson Court: 1946 - 1953
+
+#Stone Court: 1941 - 1946
+
+#Hughes Court: 1930 - 1041
+
+#Taft Court: 1921 - 1930
+
+#White Court: 1910 - 1921
+
+#Fuller Court: 1888 - 1910
+
+#Waite Court: 1874 - 1888
+
+#Chase Court: 1864 - 1873 
+
+#Taney Court: 1835 - 1864
+
+#Marshall Court: 1801 - 1835
+
+#Ellsworth Court: 1796 - 1800
+
+#Rutledge Court: 1795 - 1795
+
+#Jay Court: 1789 - 1795
 
 #elif statement to verify integer input
 
 #elif statements to break up years of courts
-
+#How to handle transitional years?
 
 
