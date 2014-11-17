68757b04a975811ab5b23b1899cd68566ec8251d
burankar <burankar@gmail.com>
2014-11-17 10:57:53 -0700

    fixed code to account for justices leaving/joining in the same year
    getting closer to correct print screen
    
    Finally got a working script with some data to use

--- SCScript.py
+++ SCScript.py
@@ -1,8 +1,26 @@
-'''This is a basic script that will output the U.S. Supreme Court makeup based on user input of a year. I'm making it to practice python objects and syntax. it will be updated with more functionality as I get more comfortable with Python.
-Author: Brian Urankar
+'''This is the first Python program I've written completely from scratch. It is a basic script that will output the U.S. Supreme Court makeup based on user input of a year. I'm making it to practice objects, syntax, proper PEP 8 conventions, and source code management. It will be updated with more functionality as I get more comfortable with Python. I don't have every justice added because...well, there are a lot of justices!
 '''
-	
-#elif statement to verify integer input
+
+'''
+The MIT License (MIT)
+Copyright (c) 2014 Brian Urankar
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
+
 
 #Justice class. Each justice is an instance of this
 class Justice(object):
@@ -12,7 +30,7 @@ class Justice(object):
 		self.begin_date = begin_date
 		self.end_date = end_date
 
-#current justices
+#justice instances
 roberts = Justice('John Roberts', 2005, 2014)
 kagan = Justice('Elena Kagan', 2010, 2014)
 sotomayor = Justice('Sonia Sotomayor', 2009, 2014)
@@ -22,35 +40,59 @@ ginsburg = Justice('Ruth Bader Ginsburg', 1994, 2014)
 thomas = Justice('Clarence Thomas', 1991, 2014)
 kennedy = Justice('Anthony Kennedy', 1988, 2014)
 scalia = Justice('Antonin Scalia', 1986, 2014)
+souter = Justice('David Souter', 1990, 2009)
+oconnor = Justice("Sandra Day O'Connor", 1981, 2006)
+stevens = Justice('John Paul Stevens', 1975, 2010)
+rehnquist = Justice('William Rehnquist', 1972, 2005)
+powell = Justice('Lewis F. Powell, Jr.', 1972, 1987)
+blackmun = Justice('Harry Blackmun', 1970, 1994)
+burger = Justice('Warren E. Burger', 1969, 1986)
+marshall = Justice('Thurgood Marshall', 1967, 1991)
+fortas = Justice('Abe Fortas', 1965, 1969)
+goldberg = Justice('Arthur Goldberg', 1962, 1965)
+white = Justice('Byron White', 1962, 1993)
 
-
-list_of_justices = [roberts, kagan, sotomayor, alito, breyer, ginsburg, thomas, kennedy, scalia]
+list_of_justices = [roberts, kagan, sotomayor, alito, breyer, ginsburg, thomas, kennedy, scalia, souter, oconnor, stevens, rehnquist, powell, blackmun, burger, marshall, fortas, goldberg, white]
 
 YEAR_INPUT = int(raw_input("Please enter year:"))
 RESULTS = []
 LEFT_COURT = []
 ENTERED_COURT = []
+INP_STR = str(YEAR_INPUT)
+
+#need something to verify integer input for year
+
+'''
+Below is the function to assemble the correct court composition. I have some lines commented out because they weren't printing correctly. I commented them out so this works but I'm keeping these lines commented out so I have an idea where I left off with the optimal code format.
+'''
 
 def calculate_court(list):
 	for x in list:
 		if YEAR_INPUT >= x.begin_date and YEAR_INPUT <= x.end_date:
 			RESULTS.append(x.name)
 	if len(RESULTS) >= 10:
-		for x in list:
+		for x in list_of_justices:
 			if YEAR_INPUT == x.end_date:
 				RESULTS.remove(x.name)
 				LEFT_COURT.append(x.name)
 			if YEAR_INPUT == x.begin_date:
 				RESULTS.remove(x.name)
 				ENTERED_COURT.append(x.name)
+		#print "The Justices in " + INP_STR + " were: %s" % tuple(RESULTS)
+		#print "Justice %s left the court in " + INP_STR % (LEFT_COURT)
+		#print "Justice %s joined the court in " + INP_STR % (ENTERED_COURT)
+	#else:
+		#print "The Justices in " + INP_STR + "were: %s" % tuple(RESULTS)
 
-print YEAR_INPUT			
+#This is the current function that actually prints in the correct format
+def correct_answer():
+	print "In " + INP_STR + " the Justices were:"
+	for x in RESULTS:
+		print x
+	for x in LEFT_COURT:
+		print "Justice " + x + " left the court in " + INP_STR
+	for x in ENTERED_COURT:
+		print "Justice " + x + " joined the court in " + INP_STR
+
 calculate_court(list_of_justices)
-print RESULTS
-print LEFT_COURT
-print ENTERED_COURT
-
-#print 'The Supreme Court Justices were: %s, %s, %s, %s, %s, %s, %s, %s, %s,' % tuple(RESULTS)
-			
-
-
+correct_answer()
